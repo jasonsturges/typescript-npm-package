50fb6196910d05c505c527384eb18cf584b2c89e
Jason Sturges <jason@jasonsturges.com>
2022-03-17 00:47:10 -0500

    Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -11,6 +11,7 @@ export default [
       name: "typescriptNpmPackage",
       file: pkg.browser,
       format: "umd",
+      sourcemap: true,
     },
     plugins: [
       resolve(),
@@ -20,16 +21,11 @@ export default [
   },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: "src/index.ts",
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
+      { file: pkg.main, format: "cjs", sourcemap: true },
+      { file: pkg.module, format: "es", sourcemap: true },
     ],
     plugins: [typescript({ tsconfig: "./tsconfig.json" })],
   },
